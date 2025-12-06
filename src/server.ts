89e77cbe29a00c34470c51e5f45e2381064cca30
Dat Le <ldat0909@gmail.com>
2025-12-06 22:42:43 +0700

    .

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,7 @@ import { startCleanupReportsJob } from './cron/cleanupReportsJob';
 
 
 const PORT = process.env.PORT || 5000;
-const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+const FRONTEND_URL = process.env.FRONTEND_URL;
 
 // Khởi tạo server từ app
 const server = http.createServer(app);
